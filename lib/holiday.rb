214604d9f6782c5034d987a429171fb3fbd56f10
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-02 01:40:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday_names|
     if season == winter
-      holiday_hash.each do |holiday_name, supplies|
+      holiday_names.each do |holiday_name, supplies|
         supplies << supply
       end
     end
