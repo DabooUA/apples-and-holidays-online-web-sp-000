98b006c257cb07d3d6e30ce0ffca1ed0bc387507
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-02 02:00:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,5 +85,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_array = []
+  holiday_hash.each do |season, holiday_supplies_hash|
+    holiday_supplies_hash.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        holiday_array << holiday
+      end
+    end
+  end
+  bbq_array
 end
